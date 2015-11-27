05768eb182de39075fb885f5f1f72ba53393f034
purewater328 <purewater328@gmail.com>
2015-11-27 14:37:24 +0900

    no message

---
+++ puzzle_solver.js
@@ -0,0 +1,17 @@
+var square={
+  squareID: getElementById(),
+  square_Info:
+};
+
+var square_array = new Array();
+for(i=0;i<9;i++)
+square_array[i]=
+
+function shuffle_square(){
+
+
+};
+
+function move_square(){
+    square.squareID
+};
